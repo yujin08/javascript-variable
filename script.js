a9e73325979c9d9464aeaa1162a26c7d41334b31
yujin08 <97872603+yujin08@users.noreply.github.com>
2022-09-01 10:23:37 +0000

    문제 2

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-// 유저가 입력하는 숫자가 0인지 음성인지 양성인지 판단하는 프로그램을 만드시오
+// 나는 대학교 교수다. 레포트 점수에따라 등급을 매기는 프로그램을 만드시오
 
-let number = 0
 
-if (number == 0) {
-  console.log("0입니다.")
+// 90~100 : A
+// 80~89 : B
+// 70~79 : C
+// 60~69 : D
+// less than 59 : F
+
+
+let score =  90
+let grade =''
+if(90<=score && score<=100){
+  grade = "A"
+}else if(80<=score && score<=89){
+  grade = "B"
+}else if(70<=score && score<=79){
+  grade ="C"
+}else if(60<=score && score<=69){
+  grade="D"
+}else if(0<=score && score<=59){
+  grade="F"
+}else {
+  console.log("잘못된 범위의 점수입니다")
 }
-else if (number > 0) {
-  console.log("양성입니다.")
-}
-else if (number < 0) {
-  console.log("음성입니다.")
-}
+console.log(grade)
