58f3f6b1a71c8eb2199d19e5cc241fdd3acbf78f
yujin08 <97872603+yujin08@users.noreply.github.com>
2022-09-01 10:26:44 +0000

    문제 3

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-// 나는 대학교 교수다. 레포트 점수에따라 등급을 매기는 프로그램을 만드시오
+// 한 지원자가 우리회사에 지원을했다. 지원자가 사용가능한 스킬은 배열에 제공이 된다
+// let skills = ["HTML","CSS","Javascript","React"]
+// Javascript와 React 둘다 할줄 안다면 “합격!” Javascript와 React 둘중 하나만 할줄 안다면 “예비”, 두 스킬이 없다면 “탈락” 을 보여주는 프로그램을 짜시오
+
+// input : let skills = ["HTML","CSS","Javascript","React"]
+// output: 합격 
+
+// input:let skills = ["HTML","CSS","Javascript"] 
+// output: 예비 
+
+// input:let skills = ["HTML","CSS"] 
+// output: 탈락
+
+let skills = ["HTML","CSS","Javascript","React"]
 
 
-// 90~100 : A
-// 80~89 : B
-// 70~79 : C
-// 60~69 : D
-// less than 59 : F
-
-
-let score =  90
-let grade =''
-if(90<=score && score<=100){
-  grade = "A"
-}else if(80<=score && score<=89){
-  grade = "B"
-}else if(70<=score && score<=79){
-  grade ="C"
-}else if(60<=score && score<=69){
-  grade="D"
-}else if(0<=score && score<=59){
-  grade="F"
+if(skills.includes("Javascript") && skills.includes("React")){
+  console.log("합격!")
+}else if(skills.includes("Javascript") || skills.includes("React")){
+  console.log("예비")
 }else {
-  console.log("잘못된 범위의 점수입니다")
+  console.log("탈락!")
 }
-console.log(grade)
